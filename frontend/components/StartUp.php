8852134636206006ca901ae060b5f668beadf052
MrAnger <slava.sharapov@mail.ru>
2017-02-22 16:34:07 +0700

    Правки конфигурации

--- frontend/components/StartUp.php
+++ frontend/components/StartUp.php
@@ -2,8 +2,6 @@
 
 namespace frontend\components;
 
-use common\models\Profile;
-use common\models\User;
 use Yii;
 use yii\base\BootstrapInterface;
 use yii\base\Event;
@@ -17,26 +15,6 @@ class StartUp implements BootstrapInterface {
 	 * @inheritDoc
 	 */
 	public function bootstrap($app) {
-		// После создания нового профиля пользователя, проверяем настроен ли емайл gravatar, если нет, то устанавливаем его принудительно
-		Event::on(Profile::className(), Profile::EVENT_AFTER_INSERT, function (AfterSaveEvent $event) {
-			/** @var Profile $profile */
-			$profile = $event->sender;
 
-			if ($profile->gravatar_email === null) {
-				$profile->gravatar_email = $profile->user->email;
-
-				$profile->save(false);
-			}
-		});
-
-		// Прописываем правильный IP адрес при создании пользователя
-		Event::on(User::className(), User::AFTER_CREATE, function (Event $event) {
-			/** @var User $user */
-			$user = $event->sender;
-
-			if (isset($_SERVER['HTTP_X_REAL_IP'])) {
-				$user->updateAttributes(['registration_ip' => $_SERVER['HTTP_X_REAL_IP']]);
-			}
-		});
 	}
 }
