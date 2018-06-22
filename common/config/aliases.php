fd4b29758f89c9e42a05a7f50b170dfe8edd2858
MrAnger <slava.sharapov@mail.ru>
2018-06-22 12:27:13 +0700

    Обновление: composer.json

--- common/config/aliases.php
+++ common/config/aliases.php
@@ -6,5 +6,8 @@ return [
 	'@backend'  => dirname(dirname(__DIR__)) . '/backend',
 	'@console'  => dirname(dirname(__DIR__)) . '/console',
 
+	'@bower' => '@vendor/bower-asset',
+	'@npm'   => '@vendor/npm-asset',
+
 	'MrAnger/Yii2_ImageManager' => dirname(dirname(__DIR__)) . '/extensions/mranger/yii2-image-manager',
 ];
