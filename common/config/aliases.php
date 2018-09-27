8aa32b0bc378626d7765565f83aa442b68dfbfbb
MrAnger <slava.sharapov@mail.ru>
2018-09-27 16:35:39 +0700

    COMMON: поправлен путь до алиаса @npm

--- common/config/aliases.php
+++ common/config/aliases.php
@@ -7,7 +7,7 @@ return [
 	'@console'  => dirname(dirname(__DIR__)) . '/console',
 
 	'@bower' => '@vendor/bower-asset',
-	'@npm'   => '@vendor/npm-asset',
+	'@npm'   => dirname(dirname(__DIR__)) . '/node_modules',
 
 	'MrAnger/Yii2_ImageManager' => dirname(dirname(__DIR__)) . '/extensions/mranger/yii2-image-manager',
 	'MrAnger/Yii2_HtmlEditorWidget' => dirname(dirname(__DIR__)) . '/extensions/mranger/yii2-html-editor-widget',
